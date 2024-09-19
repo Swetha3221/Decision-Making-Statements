c7359cc016b7696a932ea775bb79be93b5c3e897
Swetha3221 <swethagummagari@gmail.com>
2024-09-19 04:31:34 -0400

    CSEdepartment.py

---
+++ CSEdepartment.py
@@ -0,0 +1,35 @@
+'''
+'''
+There are 3 labs in the CSE department(L1, L2, and L3) with a seating capacity of x, y, and z respectively. Find the lab which has minimal seating capacity.  
+Input format:
+Input consists of 3 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+Output format:
+Print the minimal seating lab capacity
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+Sample Output:
+L3
+
+'''
+'''
+Ans:
+
+a = int(input())  # Seating capacity of L1
+b = int(input())  # Seating capacity of L2
+c = int(input())  # Seating capacity of L3
+
+
+if a < b and a < c:
+    print("L1")
+elif b < a and b < c:
+    print("L2")
+else:
+    print("L3")
+'''
+'''
